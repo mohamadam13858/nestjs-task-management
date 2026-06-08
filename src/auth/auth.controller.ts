a64909f5d23148a4327bcd16b1acd112485da5ff
mohammad <mh177148@gmail.com>
2026-06-09 00:42:50 +0330

    auth-error-handle

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
+    
     @Post('/signup')
     signup(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.authService.signUp(authCredentialsDto)
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -14,6 +14,14 @@ export class AuthService {
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto
         const user = this.userRepository.create({ username, password })
-        await this.userRepository.save(user)
+        try {
+            await this.userRepository.save(user)
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('Username already exists')
+            } else {
+                throw new InternalServerErrorException()
+            }
+        }
     }
 }
