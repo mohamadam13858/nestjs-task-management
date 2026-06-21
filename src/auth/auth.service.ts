2c76d3044f37bb5f6eb08b2b1c096d48163393a5
mohammad <mh177148@gmail.com>
2026-06-21 15:14:53 +0330

    hashedPassword

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
@@ -13,7 +14,11 @@ export class AuthService {
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto
-        const user = this.userRepository.create({ username, password })
+
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const user = this.userRepository.create({ username, password: hashedPassword })
         try {
             await this.userRepository.save(user)
         } catch (error) {
