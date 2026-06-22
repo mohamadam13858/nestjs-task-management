57baddda8df16606dcfa6fe8afdbc81aa151a5ff
mohammad <mh177148@gmail.com>
2026-06-22 14:05:38 +0330

    decorator-getuser,useguards-authguard

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [AuthModule, TypeOrmModule.forFeature([Task])],
   providers: [TasksService],
   controllers: [TasksController],
 })
-export class TasksModule {} 
+export class TasksModule { } 
