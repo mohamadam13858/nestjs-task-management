f594075316f3ac14dfd721e9c61fa044fe21bb0e
mohammad <mh177148@gmail.com>
2026-06-30 18:19:28 +0330

    .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TasksModule , TypeOrmModule.forRoot({
+  imports: [ ConfigModule.forRoot({
+    envFilePath: [`.env.stage.${process.env.STAGE}`] , 
+  }) , TasksModule , TypeOrmModule.forRoot({
     type: 'postgres' , 
     host: 'localhost' , 
     port: 5432  ,
