a0a64e9ae4327cd4e109502fcff01b3ca40c227a
mohammad <mh177148@gmail.com>
2026-07-07 15:54:02 +0330

    config.shema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.Schema';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: [`.env.stage.${process.env.STAGE}`],
+    validationSchema: configValidationSchema, 
   }), TasksModule, TypeOrmModule.forRootAsync({
     imports: [ConfigModule],
     inject: [ConfigService],
